dacf254a5ede82fb2533d1f7532fa6a035c84c96
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-29 19:26:26 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    })
+    .then(function(response){
+        return response.json()
+    }).then(function(json){
+        let p = document.createElement('p')
+        document.body.appendChild(p)
+        p.innerHTML = json.id
+    }).catch(function(error){
+        let p = document.createElement('p')
+        document.body.appendChild(p)
+        p.innerHTML = error.message
+    })
+}
